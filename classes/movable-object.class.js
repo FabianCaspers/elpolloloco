b2a2d404dcf8a4957bef358511956d3d9a3965f9
FabianCaspers <119706742+FabianCaspers@users.noreply.github.com>
2022-12-25 22:36:34 +0100

    animate character, cloud and chicken

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -1,10 +1,12 @@
 class MovableObject {
     x = 120;
-    y = 250;
+    y = 280;
     img;
     height = 150;
     width = 100;
-
+    currenImage = 0;
+    ImageCache = {};
+    speed = 0.15;
 
     // loadImage('img/test.png')
     loadImage(path) {
@@ -12,12 +14,24 @@ class MovableObject {
         this.img.src = path;
     }
 
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.ImageCache[path] = img;
+        });
+    }
+
 
     moveRight() {
         console.log('Moving right');
     }
 
-    moveLeft(){
-        
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
+
     }
+
 }
